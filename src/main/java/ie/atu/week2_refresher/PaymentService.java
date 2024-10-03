3ce41d8c3943cdccbe941c2c54655058e7f73af4
kenn <G00267173@atu.ie>
2024-10-03 20:24:58 +0100

    Lab3Feignb

--- src/main/java/ie/atu/week2_refresher/PaymentService.java
+++ src/main/java/ie/atu/week2_refresher/PaymentService.java
@@ -1,4 +1,11 @@
 package ie.atu.week2_refresher;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "Payment-Service", url = "http://localhost:8081")
 public interface PaymentService {
+    @PostMapping("/confirmMessage")
+    String paymentDetails(@RequestBody Product product);
 }
