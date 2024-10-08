4c3991c784cd7be2193df72c71d57a45bbe35563
kennyatu23 <g0267173@atu.ie>
2024-10-08 23:14:50 +0100

    branchLocal

--- src/main/java/ie/atu/week2_refresher/PaymentService.java
+++ src/main/java/ie/atu/week2_refresher/PaymentService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Payment-Service", url = "http://payment-service:8081")
+@FeignClient(name = "Payment-Service", url = "http://localhost:8081")
 public interface PaymentService {
     @PostMapping("/confirmMessage")
     String paymentDetails(@RequestBody Product product);
